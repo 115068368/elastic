75d31bdb84084d324a3ceffc01f066bef5c5483a
115068368 <115068368@163.com>
2016-06-10 21:08:59 +0800

    Initial commit

---
+++ src/com/test/test.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ * Created by Thinkpad on 2016/5/25 0025.
+ */
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.print("hello world");
+        System.out.print();
+        StringBuffer FS = new StringBuffer();
+
+    }
+
+
+}
